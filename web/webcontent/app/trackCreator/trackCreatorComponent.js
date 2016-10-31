3f25dbc01151685cbb6e5b8df5a0586670e50dec
rtaimal <rtaimal@gmail.com>
2016-10-31 00:12:40 -0500

    fixes de css transversales

--- web/webcontent/app/trackCreator/trackCreatorComponent.js
+++ web/webcontent/app/trackCreator/trackCreatorComponent.js
@@ -1,26 +1,33 @@
 var trackCreatorModule = angular.module('trackCreatorModule');
 var TrackCreatorController = ['Upload',
     function (Upload) {
-    /**
-     * Tip: add here only visual logic
-     */
-    var self = this;
-    self.uploadFilesAndData = function (files) {
-        console.log("subiendo");
-        //todo: filter  only .bex  files
-        if (files && files.length > 0) {
-            var file = files[0];
-            Upload.upload({
-                url: 'api/track/upload',
-                fields: {},
-                file: file
-            }).progress(function (evt) {
-            }).success(function (data, status, headers, config) {
-                console.log('subido correctamente');
-            });
-        }
-    };
-}];
+        /**
+         * Tip: add here only visual logic
+         */
+        var self = this;
+        self.trackFiles = {};
+        self.attachFile = function (files, fieldName) {
+            if (files && files.length > 0) {
+                var file = files[0];
+                self.trackFiles[fieldName] = file;
+            }
+        };
+        self.uploadFilesAndData = function () {
+            var self = this;
+            if (self.trackFiles) {
+                Upload.upload({
+                    url: 'api/track/upload',
+                    fields: {
+                        trackName: self.trackName
+                    },
+                    files: self.trackFiles
+                }).progress(function (evt) {
+                }).success(function (data, status, headers, config) {
+                    console.log('subido correctamente');
+                });
+            }
+        };
+    }];
 
 trackCreatorModule.component('trackCreator', {
     transclude: true,
