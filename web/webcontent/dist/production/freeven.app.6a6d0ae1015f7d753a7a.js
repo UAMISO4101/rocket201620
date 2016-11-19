f08c0c7e15b71aa1b70aedc4e4672bcb5d6c3d69
Diego F. Ruiz S <df.ruiz@uniandes.edu.co>
2016-11-19 17:35:44 -0500

    Actualización comparar fechas
    -- Se ajusta comparación de fechas al crear evento

--- web/webcontent/dist/production/freeven.app.6a6d0ae1015f7d753a7a.js
+++ web/webcontent/dist/production/freeven.app.6a6d0ae1015f7d753a7a.js
@@ -67656,6 +67656,59 @@
 	        self.files = {};
 	        self.image = true;
 
+	        // Source: http://stackoverflow.com/questions/497790
+	        var dates = {
+	            convert: function (d) {
+	                // Converts the date in d to a date-object. The input can be:
+	                //   a date object: returned without modification
+	                //  an array      : Interpreted as [year,month,day]. NOTE: month is 0-11.
+	                //   a number     : Interpreted as number of milliseconds
+	                //                  since 1 Jan 1970 (a timestamp)
+	                //   a string     : Any format supported by the javascript engine, like
+	                //                  "YYYY/MM/DD", "MM/DD/YYYY", "Jan 31 2009" etc.
+	                //  an object     : Interpreted as an object with year, month and date
+	                //                  attributes.  **NOTE** month is 0-11.
+	                return (
+	                    d.constructor === Date ? d :
+	                        d.constructor === Array ? new Date(d[0], d[1], d[2]) :
+	                            d.constructor === Number ? new Date(d) :
+	                                d.constructor === String ? new Date(d) :
+	                                    typeof d === "object" ? new Date(d.year, d.month, d.date) :
+	                                        NaN
+	                );
+	            },
+	            compare: function (a, b) {
+	                // Compare two dates (could be of any type supported by the convert
+	                // function above) and returns:
+	                //  -1 : if a < b
+	                //   0 : if a = b
+	                //   1 : if a > b
+	                // NaN : if a or b is an illegal date
+	                // NOTE: The code inside isFinite does an assignment (=).
+	                return (
+	                    isFinite(a = this.convert(a).valueOf()) &&
+	                    isFinite(b = this.convert(b).valueOf()) ?
+	                    (a > b) - (a < b) :
+	                        NaN
+	                );
+	            },
+	            inRange: function (d, start, end) {
+	                // Checks if date in d is between dates in start and end.
+	                // Returns a boolean or NaN:
+	                //    true  : if d is between start and end (inclusive)
+	                //    false : if d is before start or after end
+	                //    NaN   : if one or more of the dates is illegal.
+	                // NOTE: The code inside isFinite does an assignment (=).
+	                return (
+	                    isFinite(d = this.convert(d).valueOf()) &&
+	                    isFinite(start = this.convert(start).valueOf()) &&
+	                    isFinite(end = this.convert(end).valueOf()) ?
+	                    start <= d && d <= end :
+	                        NaN
+	                );
+	            }
+	        }
+
 
 	        self.saveEvent = function () {
 	            if (self.validateFieldsPostCreator()) {
@@ -67669,11 +67722,12 @@
 	        };
 
 	        self.validateDate = function () {
-	            var today = new Date();
-	            var isToday = (today.toDateString() == self.event.event.date.toDateString());
-	            var isLess = (today.toDateString() < self.event.event.date.toDateString());
 
-	            if (isToday == false && isLess == false) {
+	            var today = new Date();
+
+	            var howDate = dates.compare(today, new Date(self.event.event.date));
+
+	            if (howDate == -1) {
 	                return true;
 	            } else {
 	                return false;
@@ -67715,6 +67769,7 @@
 	                return true;
 	            }
 	        }
+
 
 	    }];
 
@@ -67787,6 +67842,7 @@
 	                }).success(function (data, status, headers, config) {
 	                    if (status == 201) {
 	                        notifierService.success("Eventos", "Tu evento ha sido creado");
+	                        self.event = {};
 	                        window.location.assign("#/events");
 	                    } else {
 	                        notifierService.error("Eventos", "Error creando el evento");
