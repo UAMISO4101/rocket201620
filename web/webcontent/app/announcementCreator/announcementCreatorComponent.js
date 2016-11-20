c9616982137b0588c076fc9b661a34ebb3db785b
Diego F. Ruiz S <df.ruiz@uniandes.edu.co>
2016-11-20 14:19:39 -0500

    Validación fechas en la convocatoria

--- web/webcontent/app/announcementCreator/announcementCreatorComponent.js
+++ web/webcontent/app/announcementCreator/announcementCreatorComponent.js
@@ -7,6 +7,60 @@ var AnnouncementCreatorController = ['Upload', 'mainService', 'announcementCreat
         self.items = [];
         self.selectedItem = {};
         self.showItem = false;
+
+        // Source: http://stackoverflow.com/questions/497790
+        var dates = {
+            convert: function (d) {
+                // Converts the date in d to a date-object. The input can be:
+                //   a date object: returned without modification
+                //  an array      : Interpreted as [year,month,day]. NOTE: month is 0-11.
+                //   a number     : Interpreted as number of milliseconds
+                //                  since 1 Jan 1970 (a timestamp)
+                //   a string     : Any format supported by the javascript engine, like
+                //                  "YYYY/MM/DD", "MM/DD/YYYY", "Jan 31 2009" etc.
+                //  an object     : Interpreted as an object with year, month and date
+                //                  attributes.  **NOTE** month is 0-11.
+                return (
+                    d.constructor === Date ? d :
+                        d.constructor === Array ? new Date(d[0], d[1], d[2]) :
+                            d.constructor === Number ? new Date(d) :
+                                d.constructor === String ? new Date(d) :
+                                    typeof d === "object" ? new Date(d.year, d.month, d.date) :
+                                        NaN
+                );
+            },
+            compare: function (a, b) {
+                // Compare two dates (could be of any type supported by the convert
+                // function above) and returns:
+                //  -1 : if a < b
+                //   0 : if a = b
+                //   1 : if a > b
+                // NaN : if a or b is an illegal date
+                // NOTE: The code inside isFinite does an assignment (=).
+                return (
+                    isFinite(a = this.convert(a).valueOf()) &&
+                    isFinite(b = this.convert(b).valueOf()) ?
+                    (a > b) - (a < b) :
+                        NaN
+                );
+            },
+            inRange: function (d, start, end) {
+                // Checks if date in d is between dates in start and end.
+                // Returns a boolean or NaN:
+                //    true  : if d is between start and end (inclusive)
+                //    false : if d is before start or after end
+                //    NaN   : if one or more of the dates is illegal.
+                // NOTE: The code inside isFinite does an assignment (=).
+                return (
+                    isFinite(d = this.convert(d).valueOf()) &&
+                    isFinite(start = this.convert(start).valueOf()) &&
+                    isFinite(end = this.convert(end).valueOf()) ?
+                    start <= d && d <= end :
+                        NaN
+                );
+            }
+        }
+
         self.attachFile = function (files, fieldName) {
             if (files && files.length > 0) {
                 var file = files[0];
@@ -16,7 +70,11 @@ var AnnouncementCreatorController = ['Upload', 'mainService', 'announcementCreat
         self.showItemView = function () {
             var self = this;
             if (self.validateFields()) {
-                self.showItem = true;
+                if (self.validateDate()) {
+                    self.showItem = true;
+                } else {
+                    notifierService.error("Error en las fechas", "Verifique las fechas por favor.");
+                }
             } else {
                 notifierService.error("Error Convocatoria", "Faltan campos por diligenciar");
             }
@@ -133,6 +191,18 @@ var AnnouncementCreatorController = ['Upload', 'mainService', 'announcementCreat
             if (self.selectedItem.description != undefined
                 && self.selectedItem.name != undefined &&
                 self.selectedItem.gender != undefined) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+        self.validateDate = function () {
+            var today = new Date();
+            var howDateStart = dates.compare(today, new Date(self.start_date));
+            var howDateEnd = dates.compare(today, new Date(self.end_date));
+            var howDateOk = dates.compare(new Date(self.start_date), new Date(self.end_date));
+            if (howDateStart == -1 && howDateEnd == -1 && howDateOk == -1) {
                 return true;
             } else {
                 return false;
